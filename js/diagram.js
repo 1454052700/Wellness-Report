dce8ce2a8cd384c5ffbc79e5b6efc09cf879f46a
wangyi <1454052700@qq.com>
2020-08-11 18:30:19 +0800

    修改

--- js/diagram.js
+++ js/diagram.js
@@ -15,6 +15,9 @@ function diagram(dome, data, color, dataTwo) {
         exporting: {
             enabled: false
         },
+        lang:{
+            noData: "暂无数据"
+        },
         title: {
             text: null
         },
@@ -51,6 +54,75 @@ function diagram(dome, data, color, dataTwo) {
                     fontSize: '16px'
                 }
             }
+
+        },
+        series: [{ data: data }, { data: dataTwo }]
+    });
+}
+
+function diagram2(dome, data, color, dataTwo) {
+    console.log(data, "data")
+
+    let xdata = [];
+    data.forEach(i => {
+        xdata.push(i.name)
+    })
+    console.log(xdata, "xdata")
+    var chart = Highcharts.chart(dome, {
+        chart: {
+            type: 'area',
+            plotBackgroundColor: '#fff'
+        },
+        lang:{
+            noData: "暂无数据"
+        },
+        tooltip: {
+            formatter: function () {
+                if (this.series.name == "Series 2") {
+                    return this.x + '<br/><br/>' +'<strong>' + "入睡时间 " + '</strong>' + 
+                        Highcharts.dateFormat('%H:%M', this.y);
+                } else {
+                    return this.x + '<br/><br/>' +'<strong>' + "起床时间 " + '</strong>' + 
+                        Highcharts.dateFormat('%H:%M', this.y);
+                }
+            },
+        },
+        exporting: {
+            enabled: false
+        },
+        title: {
+            text: null
+        },
+        credits: {
+            enabled: false
+        },
+        colors: [color],
+        // colors: ["#61C2F3","rgba(128,123,233)"],
+        plotOptions: {
+            area: {
+                fillOpacity: .1
+            }
+        },
+        legend: {
+            enabled: false
+        },
+        yAxis: {
+            type: 'datetime',
+            title: null,
+            labels: {
+                formatter: function () {
+                    return Highcharts.dateFormat('%H:%M', this.value);
+                }
+            }
+        },
+        xAxis: {
+            labels: {
+                style: {
+                    color: '#9FA3AE',
+                    fontSize: '16px'
+                }
+            },
+            categories: xdata
 
         },
         series: [{ data: data }, { data: dataTwo }]
